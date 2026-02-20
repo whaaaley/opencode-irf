ab0d496b83edc1608f30dc9121957f9ff6b50f93
Dustin Dowell <7419947+whaaaley@users.noreply.github.com>
2026-02-20 02:32:33 -0600

    refactor: simplify shell.nix to use unpinned nixos-unstable

--- shell.nix
+++ shell.nix
@@ -1,16 +1,9 @@
-{ pkgs ? import <nixpkgs> {} }:
-
 let
-  unstable = import (fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz";
-    sha256 = "sha256:1cnb74cr9zz88430xy1m9f7dxyx6237qwpljnqy62m6xjx264r9b";
-  }) {
-    inherit (pkgs) system;
-    config = pkgs.config;
-  };
-in pkgs.mkShell {
-  buildInputs = with pkgs; [
-    unstable.deno
+  tarball = fetchTarball { url = "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz"; };
+  nixpkgs = import tarball {};
+in with nixpkgs; mkShell {
+  buildInputs = [
+    deno
     lazygit
     nodejs_22
     tmux
